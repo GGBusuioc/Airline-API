636e0f9958c36fd89f7116aa3127d9cddea32e03
George-Gabriel <sc15ggb@leeds.ac.uk>
2018-03-02 10:40:25 +0000

    Admin interface

--- airline/airlineapp/admin.py
+++ airline/airlineapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Aircraft)
+admin.site.register(Airport)
+admin.site.register(Flight)
+admin.site.register(Booking)
+admin.site.register(Passenger)
+admin.site.register(PaymentProvider)
+admin.site.register(Invoice)
