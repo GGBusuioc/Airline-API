4e7220204d7d88331a17cada7669d0b288986e71
George-Gabriel <sc15ggb@leeds.ac.uk>
2018-03-02 10:18:58 +0000

    First database migration. Some fields missing in the Invoice table

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'airlineapp.apps.AirlineappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
